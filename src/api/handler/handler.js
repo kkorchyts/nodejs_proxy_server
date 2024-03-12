65df37fd6936ab44c57a7db33d36dc301a02edce
kkorchyts <kkorchyts@gmail.com>
2024-03-12 11:29:44 +0100

    Proxy-9: implement task

--- src/api/handler/handler.js
+++ src/api/handler/handler.js
@@ -1,9 +1,17 @@
-import {axiosNasaClient} from "../../clients/index.js";
+import {nasaAsteroidService} from "../../services/index.js";
 import {getPreviousPeriod, parseDate} from "./dateUtils.js";
 
+const parseQueryParams = (query) => {
+    return {
+        date: query.date,
+        countOnly: Boolean(query.count),
+        dangerousOnly: Boolean(query["were-dangerous-meteors"]),
+    }
+}
+
 export const handler = async (req, res) => {
-    const dateQuery = req.query.date;
-    const {errorMessage, date} = parseDate(dateQuery);
+    const {date, countOnly, dangerousOnly} = parseQueryParams(req.query);
+    const {errorMessage} = parseDate(date);
     if (errorMessage) {
         res.status(400).send(errorMessage);
         return;
@@ -11,7 +19,7 @@ export const handler = async (req, res) => {
 
     try {
         const period = getPreviousPeriod(date);
-        const response = await axiosNasaClient.getAsteroidsCountByPeriod(period);
+        const response = await nasaAsteroidService.getAsteroidsCountByPeriod(period, countOnly, dangerousOnly);
         res.set("Content-Type", "application/json")
         res.send(JSON.stringify(response, null, 2));
     } catch (error) {
---
+++ src/services/nasa/nasa-asteroids.service.js
@@ -0,0 +1,48 @@
+import {axiosNasaClient} from "../../clients/index.js";
+
+const parseAsteroidsToArray = (dataArray, dangerousOnly) => {
+    const result = [];
+    dataArray.forEach((data) => {
+        if (!dangerousOnly || data.is_potentially_hazardous_asteroid === true)
+            result.push({
+                id: data.id,
+                name: data.name,
+                estimated_diameter_in_meters: {
+                    estimated_diameter_min: data.estimated_diameter.meters.estimated_diameter_min,
+                    estimated_diameter_max: data.estimated_diameter.meters.estimated_diameter_max
+                },
+                is_potentially_hazardous_asteroid: data.is_potentially_hazardous_asteroid,
+                close_approach_date_full: data.close_approach_data[0].close_approach_date_full,
+                relative_velocity_kilometers_per_second: data.close_approach_data[0].relative_velocity.kilometers_per_second,
+            });
+    })
+    return result;
+}
+
+const parseAsteroidsToCount = (dataArray, dangerousOnly) => {
+    let count = 0;
+    dataArray.forEach((data) => {
+        if (!dangerousOnly || data.is_potentially_hazardous_asteroid === true) {
+            count++
+        }
+    })
+    return {count};
+}
+
+const parseNasaAsteroidsData = (data, countOnly, dangerousOnly) => {
+    const result = {};
+    const asteroidArrayParser = countOnly ? parseAsteroidsToCount : parseAsteroidsToArray;
+    Object.keys(data.near_earth_objects)
+        .forEach(
+            (key) =>
+                result[key] = asteroidArrayParser(data.near_earth_objects[key], dangerousOnly)
+        );
+    return result;
+}
+
+export class NasaAsteroidsService {
+    async getAsteroidsCountByPeriod(period, countOnly, dangerousOnly) {
+        const asteroidsData = await axiosNasaClient.getAsteroidsCountByPeriod(period);
+        return parseNasaAsteroidsData(asteroidsData, countOnly, dangerousOnly);
+    }
+}
