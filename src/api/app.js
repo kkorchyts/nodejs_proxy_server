edeb513c6829b2c3b5db018139a2e818cb914a20
kkorchyts <kkorchyts@gmail.com>
2024-03-12 18:10:19 +0100

    Proxy-9: fix comments

--- src/api/app.js
+++ src/api/app.js
@@ -1,8 +1,8 @@
 import express from "express"
 import {nasaAsteroidsHandler} from "./handler/nasa-asteroids.handler.js";
 import {checkQueryParamMiddleware} from "./middleware/nasa-asteroids.middleware.js";
-import {errorMiddleware} from "./middleware/error.middleware.js";
+import {exceptionFilterMiddleware} from "./middleware/exception-filter.middleware.js";
 
 export const app = express();
 app.get('/meteors', checkQueryParamMiddleware, nasaAsteroidsHandler)
-app.use(errorMiddleware);
+app.use(exceptionFilterMiddleware);
--- src/api/middleware/nasa-asteroids.middleware.js
+++ src/api/middleware/nasa-asteroids.middleware.js
@@ -1,11 +1,11 @@
 import {validateDate} from "../utils/date-utils.js";
-
+import {Exception} from "./Exception.js";
 
 export const checkQueryParamMiddleware = (req, res, next) => {
     const {date} = req.query;
     const {errorMessage} = validateDate(date);
     if (errorMessage) {
-        return res.status(400).send(errorMessage);
+       throw new Exception(400, errorMessage);
     }
     next();
 }
