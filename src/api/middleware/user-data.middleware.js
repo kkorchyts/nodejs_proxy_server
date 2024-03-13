8921b70e092fb41df1c1801b26656796f8669bb6
kkorchyts <kkorchyts@gmail.com>
2024-03-13 21:58:20 +0100

    Proxy-11: fix comments

--- src/api/middleware/user-data.middleware.js
+++ src/api/middleware/user-data.middleware.js
@@ -1,16 +1,14 @@
 import {Exception} from "../exceptions/Exception.js";
 
-function validateStringNotEmpty(string, errorMessagePrefix, errors) {
+function validateStringNotEmpty(string, errorMessagePrefix) {
     if (!string || string.trim().length === 0) {
-        errors.push(`${errorMessagePrefix} is empty`);
+        return `${errorMessagePrefix} is empty`;
     }
 }
 
 export const checkUserDataMiddleware = (req, res, next) => {
     const userData = req.body;
-    const errors = [];
-    validateStringNotEmpty(userData.id, "User id", errors);
-    validateStringNotEmpty(userData.name, "User name", errors);
+    const errors = [validateStringNotEmpty(userData.id, "User id"), validateStringNotEmpty(userData.name, "User name")].filter(x => x);
     if (errors.length > 0) {
         const exception = new Exception(400, "User data didn't passed validation");
         exception.errors = errors;
