8be255bb28fe7d47ee4596ce9be6e2d5c906147f
kkorchyts <kkorchyts@gmail.com>
2024-03-17 22:40:15 +0100

    Proxy-10: Refactor code

--- public/scripts/user-form.js
+++ public/scripts/user-form.js
@@ -1,35 +1,35 @@
 const showMessage = (isSuccessfulResponse) => {
     if (isSuccessfulResponse) {
-        document.getElementById('errorMessage').style.display = 'none';
-        document.getElementById('successMessage').style.display = 'block';
+        document.getElementById("errorMessage").style.display = "none";
+        document.getElementById("successMessage").style.display = "block";
     } else {
-        document.getElementById('successMessage').style.display = 'none';
-        document.getElementById('errorMessage').textContent = 'Failed to submit form';
-        document.getElementById('errorMessage').style.display = 'block';
+        document.getElementById("successMessage").style.display = "none";
+        document.getElementById("errorMessage").textContent = "Failed to submit form";
+        document.getElementById("errorMessage").style.display = "block";
     }
-}
+};
 const submitForm = (event) => {
     event.preventDefault();
     const formData = {
-        id: document.getElementById('id').value,
-        name: document.getElementById('name').value
+        id: document.getElementById("id").value,
+        name: document.getElementById("name").value
     };
-    fetch('/api/user', {
-        method: 'POST',
+    fetch("/api/user", {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(formData)
     })
         .then(response => {
-            showMessage(response.ok)
+            showMessage(response.ok);
         })
         .catch(error => {
             showMessage(false);
         });
-}
+};
 
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('userDataForm');
-    form.addEventListener('submit', submitForm);
-})
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("userDataForm");
+    form.addEventListener("submit", submitForm);
+});
